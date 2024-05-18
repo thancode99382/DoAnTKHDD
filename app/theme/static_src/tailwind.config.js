394df48a38da055c7a6c8cec7858893226433cfb
thancode99382 <dat99382@donga.edu.vn>
2024-05-18 19:58:27 +0700

    fix tailwind

--- app/theme/static_src/tailwind.config.js
+++ app/theme/static_src/tailwind.config.js
@@ -42,10 +42,7 @@ module.exports = {
         // '../../**/*.py'
     ],
     theme: {
-        extend: {
-           
-        },
-        
+        extend: {},
     },
     plugins: [
         /**
